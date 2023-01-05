990b846279c74af5b9375d976070b9857bd33a6c
Rolando Sotelo <rola@hey.com>
2023-01-05 15:00:12 +0100

    chore: configure jest with babel

---
+++ tests/classes/Item.test.ts
@@ -0,0 +1,17 @@
+import Item from "../../src/classes/Item.js";
+import {ItemId} from "../../src/common/enums.js";
+
+describe("Item class", () => {
+    describe("When item is created", () => {
+        const id = ItemId.CORN;
+        const turn = 1;
+        const item = new Item(id, turn);
+        test("then it should have proper id and turn", () => {
+        expect(item.id).toBe(id);
+        expect(item.turn).toBe(turn);
+        });
+        test("then it should have proper stringifyItem", () => {
+        expect(item.stringifyItem).toBe(`${id}@${turn}`);
+        });
+    });
+});
